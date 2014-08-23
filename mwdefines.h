2ceb009aef233967b7180a8229d97298c96df4cf
root <you@example.com>
2014-08-22 21:51:26 -0400

    ADD MW C project Headersand a useless .txt

---
+++ mwdefines.h
@@ -0,0 +1,13 @@
+/* MW USEFUL & BEAUTIFULL VALUE DEFINITION */
+/* BOOLEAN VALUES */
+#define ON 1
+#define OFF 0
+#define TRUE 1
+#define FALSE 0
+/* S.O. CODES */
+#define WINDOWS 1
+#define LINUX 2
+#define OSX 3
+#define SOLARIS 4
+#define UNIX 5
+#define UNKNOWN 0
